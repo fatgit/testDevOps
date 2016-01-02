c3c066162a3c9322ae26ae8388ea58e1af05af4a
Alexey Fominykh <alexey.fominyh@gmail.com>
2016-01-02 20:13:53 +0200

    some fixes

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 from flask import Flask, render_template, request, json
+import subprocess
 
 application = Flask(__name__)
 
@@ -9,7 +10,39 @@ application = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@application.route('/app', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def app():
+    print(request.method    )
+    return 'ssss'
 
+
+@application.route('/skills', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def skills():
+    f = open('application/skills', 'r')
+    skiill = f.read()
+    print(skiill)
+    return "skiill"
+
+
+
+
+@application.route('/messages', methods = ['POST'])
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type ;)"
 
 if __name__=="__main__":
     # application.run(debug=True)
