37a143b6a0cb0766164c9a700ab3d19b65b02bd1
Alexey Fominykh <alexey.fominyh@gmail.com>
2016-01-04 09:44:10 +0200

    some fixes

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import os
 from flask import Flask, render_template, request, json
-import subprocess
 
 application = Flask(__name__)
 
@@ -10,17 +8,12 @@ application = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@application.route('/app', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
-def app():
-    print(request.method)
-    return 'ssss'
-
 
 @application.route('/skills', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
 def skills():
     print (request.method)
     if request.method == 'GET':
-        f = open('application/skills', 'r')
+        f = open('/data/skills', 'r')
         skills = f.read()
         file_split = skills.split('\n')
         print(file_split)
@@ -33,7 +26,7 @@ def skills():
 
     elif request.method == 'POST':
         skill = request.form['name']
-        f = open('application/skills', 'a')
+        f = open('/data/skills', 'a')
         f.write("\n"+skill)
         f.close
 
@@ -42,20 +35,21 @@ def skills():
 
     elif request.method == 'PUT':
         name = request.form['name']
-        f = open('application/skills', 'w')
+        f = open('/data/skills', 'w')
         f.write(name)
         f.close()
 
         return json.dumps({'answer': 'Save'})
 
     elif request.method == 'DELETE':
-        f = open('application/skills', 'w')
+        f = open('/data/skills', 'w')
         f.write('')
         f.close()
 
         return json.dumps({'answer': 'All skills have been removed'})
 
-    return "skiill"
+    else:
+        return json.dumps({'answer': '404'})
 
 
 if __name__=="__main__":
